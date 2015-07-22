47226250664a5b834bbcd49190c9eb959bb90458
Sterling <Apocolyptus@users.noreply.github.com>
2015-07-21 23:30:45 -0700

    Update KingdomGame.java

--- src/com/game/KingdomGame.java
+++ src/com/game/KingdomGame.java
@@ -35,18 +35,21 @@ public class KingdomGame implements Serializable {
     }
     
     public class Countdown {
-    public static void main(String[] args) throws InterruptedException {
-        int time = Integer.parseInt(args[0]);
-        Queue<Integer> queue = new LinkedList<Integer>();
-
-        for (int i = time; i >= 0; i--)
-            queue.add(i);
-
-        while (!queue.isEmpty()) {
-            System.out.println(queue.remove());
-            Thread.sleep(1000);
+        public static void main(String[] args) throws InterruptedException {
+            int time = Integer.parseInt(args[0]);
+            Queue<Integer> queue = new LinkedList<Integer>();
+    
+            for (int i = time; i >= 0; i--)
+                queue.add(i);
+    
+            while (!queue.isEmpty()) {
+                System.out.println(queue.remove());
+                Thread.sleep(1000);
+            }
         }
     }
-}
+    
+    
+    
 
 } //End
