1da62c7d6ba3c3235aee5b0dd90966a8d9817e59
Sterling <Apocolyptus@users.noreply.github.com>
2015-07-21 22:35:06 -0700

    Update Time.java

--- src/com/game/view/Time.java
+++ src/com/game/view/Time.java
@@ -4,8 +4,14 @@ public class Time {
     
     public static void waitTime(long millisecond) {
         long max = millisecond;
+        
         for(long i = 0; i < max; i++) {
-            for(long j = 0; j < max; j++) {}
+            
+            for(long j = 0; j < max; j++) {
+                
+                Thread.sleep(1000);
+                System.out.println(i);
+            }
         }
     }
 }
